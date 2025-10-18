7299d1bb0eb404b8372b6654d9360eeda9ec81d3
andrew wsl <andrew.zh7204126@gmail.com>
2025-10-18 10:26:21 +0800

    bandurate 38400

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-
 #include "stm32f4xx_hal.h"
 #include "yaskawa.h"
 #include <stdio.h>
@@ -7,10 +6,245 @@
 // 系统时钟配置
 void SystemClock_Config(void);
 void MX_GPIO_Init(void);
-void MX_UART4_Init(void);
+void MX_USART1_UART_Init(void);  // 改为USART1
 void Error_Handler(void);
 
-UART_HandleTypeDef huart4;
+UART_HandleTypeDef UART1_Handler;  // 改为USART1
+
+
+//时钟系统配置函数
+//Fvco=Fs*(plln/pllm);
+//SYSCLK=Fvco/pllp=Fs*(plln/(pllm*pllp));
+//Fusb=Fvco/pllq=Fs*(plln/(pllm*pllq));
+
+//Fvco:VCO频率
+//SYSCLK:系统时钟频率
+//Fusb:USB,SDIO,RNG等的时钟频率
+//Fs:PLL输入时钟频率,可以是HSI,HSE等. 
+//plln:主PLL倍频系数(PLL倍频),取值范围:64~432.
+//pllm:主PLL和音频PLL分频系数(PLL之前的分频),取值范围:2~63.
+//pllp:系统时钟的主PLL分频系数(PLL之后的分频),取值范围:2,4,6,8.(仅限这4个值!)
+//pllq:USB/SDIO/随机数产生器等的主PLL分频系数(PLL之后的分频),取值范围:2~15.
+
+//外部晶振为8M的时候,推荐值:plln=336,pllm=8,pllp=2,pllq=7.
+//得到:Fvco=8*(336/8)=336Mhz
+//     SYSCLK=336/2=168Mhz
+//     Fusb=336/7=48Mhz
+//返回值:0,成功;1,失败
+void Stm32_Clock_Init(uint32_t plln,uint32_t pllm,uint32_t pllp,uint32_t pllq)
+{
+    HAL_StatusTypeDef ret = HAL_OK;
+    RCC_OscInitTypeDef RCC_OscInitStructure; 
+    RCC_ClkInitTypeDef RCC_ClkInitStructure;
+    
+    __HAL_RCC_PWR_CLK_ENABLE(); //使能PWR时钟
+    
+    //下面这个设置用来设置调压器输出电压级别，以便在器件未以最大频率工作
+    //时使性能与功耗实现平衡。
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);//设置调压器输出电压级别1
+    
+    RCC_OscInitStructure.OscillatorType=RCC_OSCILLATORTYPE_HSE;    //时钟源为HSE
+    RCC_OscInitStructure.HSEState=RCC_HSE_ON;                      //打开HSE
+    RCC_OscInitStructure.PLL.PLLState=RCC_PLL_ON;//打开PLL
+    RCC_OscInitStructure.PLL.PLLSource=RCC_PLLSOURCE_HSE;//PLL时钟源选择HSE
+    RCC_OscInitStructure.PLL.PLLM=pllm; //主PLL和音频PLL分频系数(PLL之前的分频),取值范围:2~63.
+    RCC_OscInitStructure.PLL.PLLN=plln; //主PLL倍频系数(PLL倍频),取值范围:64~432.  
+    RCC_OscInitStructure.PLL.PLLP=pllp; //系统时钟的主PLL分频系数(PLL之后的分频),取值范围:2,4,6,8.(仅限这4个值!)
+    RCC_OscInitStructure.PLL.PLLQ=pllq; //USB/SDIO/随机数产生器等的主PLL分频系数(PLL之后的分频),取值范围:2~15.
+    ret=HAL_RCC_OscConfig(&RCC_OscInitStructure);//初始化
+	
+    if(ret!=HAL_OK) while(1);
+    
+    //选中PLL作为系统时钟源并且配置HCLK,PCLK1和PCLK2
+    RCC_ClkInitStructure.ClockType=(RCC_CLOCKTYPE_SYSCLK|RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2);
+    RCC_ClkInitStructure.SYSCLKSource=RCC_SYSCLKSOURCE_PLLCLK;//设置系统时钟时钟源为PLL
+    RCC_ClkInitStructure.AHBCLKDivider=RCC_SYSCLK_DIV1;//AHB分频系数为1
+    RCC_ClkInitStructure.APB1CLKDivider=RCC_HCLK_DIV4; //APB1分频系数为4
+    RCC_ClkInitStructure.APB2CLKDivider=RCC_HCLK_DIV2; //APB2分频系数为2
+    ret=HAL_RCC_ClockConfig(&RCC_ClkInitStructure,FLASH_LATENCY_5);//同时设置FLASH延时周期为5WS，也就是6个CPU周期。
+		
+    if(ret!=HAL_OK) while(1);
+
+	 //STM32F405x/407x/415x/417x Z版本的器件支持预取功能
+	if (HAL_GetREVID() == 0x1001)
+	{
+		__HAL_FLASH_PREFETCH_BUFFER_ENABLE();  //使能flash预取
+	}
+}
+
+#define USART_REC_LEN  			200  	//定义最大接收字节数 200
+#define EN_USART1_RX 			1		//使能（1）/禁止（0）串口1接收
+	  	
+extern uint8_t  USART_RX_BUF[USART_REC_LEN]; //接收缓冲,最大USART_REC_LEN个字节.末字节为换行符 
+extern uint16_t USART_RX_STA;         		//接收状态标记	
+extern UART_HandleTypeDef UART1_Handler; //UART句柄
+
+#define RXBUFFERSIZE   1 //缓存大小
+extern uint8_t aRxBuffer[RXBUFFERSIZE];//HAL库USART接收Buffer
+
+//加入以下代码,支持printf函数,而不需要选择use MicroLIB	  
+//#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)	
+#if 1
+#pragma import(__use_no_semihosting)             
+//标准库需要的支持函数                 
+struct __FILE 
+{ 
+	int handle; 
+}; 
+
+FILE __stdout;       
+//定义_sys_exit()以避免使用半主机模式    
+void _sys_exit(int x) 
+{ 
+	x = x; 
+} 
+//重定义fputc函数 
+int fputc(int ch, FILE *f)
+{ 	
+	while((USART1->SR&0X40)==0);//循环发送,直到发送完毕   
+	USART1->DR = (uint8_t) ch;      
+	return ch;
+}
+#endif 
+
+//串口1中断服务程序
+//注意,读取USARTx->SR能避免莫名其妙的错误   	
+uint8_t USART_RX_BUF[USART_REC_LEN];     //接收缓冲,最大USART_REC_LEN个字节.
+//接收状态
+//bit15，	接收完成标志
+//bit14，	接收到0x0d
+//bit13~0，	接收到的有效字节数目
+uint16_t USART_RX_STA=0;       //接收状态标记	
+
+uint8_t aRxBuffer[RXBUFFERSIZE];//HAL库使用的串口接收缓冲
+UART_HandleTypeDef UART1_Handler; //UART句柄
+
+//初始化IO 串口1 
+//bound:波特率
+void uart_init(uint32_t bound)
+{	
+	//UART 初始化设置
+	UART1_Handler.Instance=USART1;					    //USART1
+	UART1_Handler.Init.BaudRate=bound;				    //波特率
+	UART1_Handler.Init.WordLength=UART_WORDLENGTH_8B;   //字长为8位数据格式
+	UART1_Handler.Init.StopBits=UART_STOPBITS_1;	    //一个停止位
+	UART1_Handler.Init.Parity=UART_PARITY_NONE;		    //无奇偶校验位
+	UART1_Handler.Init.HwFlowCtl=UART_HWCONTROL_NONE;   //无硬件流控
+	UART1_Handler.Init.Mode=UART_MODE_TX_RX;		    //收发模式
+	HAL_UART_Init(&UART1_Handler);					    //HAL_UART_Init()会使能UART1
+	
+	HAL_UART_Receive_IT(&UART1_Handler, (uint8_t *)aRxBuffer, RXBUFFERSIZE);//该函数会开启接收中断：标志位UART_IT_RXNE，并且设置接收缓冲以及接收缓冲接收最大数据量
+  
+}
+
+//UART底层初始化，时钟使能，引脚配置，中断配置
+//此函数会被HAL_UART_Init()调用
+//huart:串口句柄
+
+void HAL_UART_MspInit(UART_HandleTypeDef *huart)
+{
+    //GPIO端口设置
+	GPIO_InitTypeDef GPIO_Initure;
+	
+	if(huart->Instance==USART1)//如果是串口1，进行串口1 MSP初始化
+	{
+		__HAL_RCC_GPIOA_CLK_ENABLE();			//使能GPIOA时钟
+		__HAL_RCC_USART1_CLK_ENABLE();			//使能USART1时钟
+	
+		GPIO_Initure.Pin=GPIO_PIN_9;			//PA9
+		GPIO_Initure.Mode=GPIO_MODE_AF_PP;		//复用推挽输出
+		GPIO_Initure.Pull=GPIO_PULLUP;			//上拉
+		GPIO_Initure.Speed=GPIO_SPEED_FAST;		//高速
+		GPIO_Initure.Alternate=GPIO_AF7_USART1;	//复用为USART1
+		HAL_GPIO_Init(GPIOA,&GPIO_Initure);	   	//初始化PA9
+
+		GPIO_Initure.Pin=GPIO_PIN_10;			//PA10
+		HAL_GPIO_Init(GPIOA,&GPIO_Initure);	   	//初始化PA10
+		
+		HAL_NVIC_EnableIRQ(USART1_IRQn);				//使能USART1中断通道
+		HAL_NVIC_SetPriority(USART1_IRQn,3,3);			//抢占优先级3，子优先级3
+	}
+
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	if(huart->Instance==USART1)//如果是串口1
+	{
+		if((USART_RX_STA&0x8000)==0)//接收未完成
+		{
+			if(USART_RX_STA&0x4000)//接收到了0x0d
+			{
+				if(aRxBuffer[0]!=0x0a)USART_RX_STA=0;//接收错误,重新开始
+				else USART_RX_STA|=0x8000;	//接收完成了 
+			}
+			else //还没收到0X0D
+			{	
+				if(aRxBuffer[0]==0x0d)USART_RX_STA|=0x4000;
+				else
+				{
+					USART_RX_BUF[USART_RX_STA&0X3FFF]=aRxBuffer[0] ;
+					USART_RX_STA++;
+					if(USART_RX_STA>(USART_REC_LEN-1))USART_RX_STA=0;//接收数据错误,重新开始接收	  
+				}		 
+			}
+		}
+
+	}
+}
+ 
+//串口1中断服务程序
+void USART1_IRQHandler(void)                	
+{ 
+	uint32_t timeout=0;
+#if SYSTEM_SUPPORT_OS	 	//使用OS
+	OSIntEnter();    
+#endif
+	
+	HAL_UART_IRQHandler(&UART1_Handler);	//调用HAL库中断处理公用函数
+	
+	timeout=0;
+    while (HAL_UART_GetState(&UART1_Handler) != HAL_UART_STATE_READY)//等待就绪
+	{
+	 timeout++;////超时处理
+     if(timeout>HAL_MAX_DELAY) break;		
+	
+	}
+     
+	timeout=0;
+	while(HAL_UART_Receive_IT(&UART1_Handler, (uint8_t *)aRxBuffer, RXBUFFERSIZE) != HAL_OK)//一次处理完成之后，重新开启中断并设置RxXferCount为1
+	{
+	 timeout++; //超时处理
+	 if(timeout>HAL_MAX_DELAY) break;	
+	}
+#if SYSTEM_SUPPORT_OS	 	//使用OS
+	OSIntExit();  											 
+#endif
+} 
+
+//初始化延迟函数
+//当使用ucos的时候,此函数会初始化ucos的时钟节拍
+//SYSTICK的时钟固定为AHB时钟
+//SYSCLK:系统时钟频率
+static uint32_t fac_us=0;							//us延时倍乘数
+void delay_init(uint32_t SYSCLK)
+{
+#if SYSTEM_SUPPORT_OS 						//如果需要支持OS.
+	u32 reload;
+#endif
+    HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);//SysTick频率为HCLK
+	fac_us=SYSCLK;						//不论是否使用OS,fac_us都需要使用
+#if SYSTEM_SUPPORT_OS 						//如果需要支持OS.
+	reload=SYSCLK;					    //每秒钟的计数次数 单位为K	   
+	reload*=1000000/delay_ostickspersec;	//根据delay_ostickspersec设定溢出时间
+											//reload为24位寄存器,最大值:16777216,在180M下,约合0.745s左右	
+	fac_ms=1000/delay_ostickspersec;		//代表OS可以延时的最少单位	   
+	SysTick->CTRL|=SysTick_CTRL_TICKINT_Msk;//开启SYSTICK中断
+	SysTick->LOAD=reload; 					//每1/OS_TICKS_PER_SEC秒中断一次	
+	SysTick->CTRL|=SysTick_CTRL_ENABLE_Msk; //开启SYSTICK
+#else
+#endif
+}			
 
 int main(void)
 {
@@ -18,41 +252,55 @@ int main(void)
     HAL_Init();
 
     // 配置系统时钟
-    SystemClock_Config();
+    //SystemClock_Config();
+    Stm32_Clock_Init(336,8,2,7); //System clock 168MHz
 
     // 初始化GPIO
     MX_GPIO_Init();
 
-    // 初始化UART4
-    MX_UART4_Init();
+	delay_init(168);               	//初始化延时函数
+    // 初始化USART1
+	uart_init(115200);              //初始化USART
 
     // 初始化Yaskawa编码器通信
-    yaskawa_init();
+    //yaskawa_init();
 
     char msg[] = "STM32F407 Yaskawa Encoder Reader Started\r\n";
-    HAL_UART_Transmit(&huart4, (uint8_t*)msg, strlen(msg), 1000);
+    HAL_UART_Transmit(&UART1_Handler, (uint8_t*)msg, strlen(msg), 1000);  // 改为UART1_Handler
+    while(__HAL_UART_GET_FLAG(&UART1_Handler,UART_FLAG_TC)!=SET);		//等待发送结束
 
     while (1)
     {
         // 更新编码器数据
-        yaskawa_update();
+       // yaskawa_update();
 
-        // 获取编码器位置
-        float position = yaskawa_get_position();
-        uint32_t error_count = yaskawa_get_error_count();
-        uint32_t crc_ok = yaskawa_get_crc_ok();
-        uint32_t crc_error = yaskawa_get_crc_error();
+        // 获取编码器位置及状态
+        float position = 0;
+        uint32_t error_count = 0;
+        uint32_t crc_ok = 0;
+        uint32_t crc_error = 0;
+
+        
+        //position = yaskawa_get_position();
+        //error_count = yaskawa_get_error_count();
+        //crc_ok = yaskawa_get_crc_ok();
+        //crc_error = yaskawa_get_crc_error();
 
         // 通过串口输出编码器数据
         char buffer[200];
-        snprintf(buffer, sizeof(buffer), 
-                "Pos: %.6f rad, Errors: %lu, CRC OK: %lu, CRC Err: %lu\r\n", 
+        snprintf(buffer, sizeof(buffer),
+                "Pos: %.6f rad, Errors: %lu, CRC OK: %lu, CRC Err: %lu\r\n",
                 position, error_count, crc_ok, crc_error);
+        while(__HAL_UART_GET_FLAG(&UART1_Handler,UART_FLAG_TC)!=SET);		//等待发送结束
+        //HAL_UART_Transmit(&UART1_Handler, (uint8_t*)buffer, strlen(buffer), 1000);  // 改为UART1_Handler
 
-        HAL_UART_Transmit(&huart4, (uint8_t*)buffer, strlen(buffer), 1000);
+        // LED翻转，实现闪烁
+        HAL_GPIO_TogglePin(GPIOF, GPIO_PIN_9);
+        HAL_GPIO_TogglePin(GPIOF, GPIO_PIN_10);
 
         HAL_Delay(100); // 100ms刷新率
     }
+
 }
 
 void SystemClock_Config(void)
@@ -89,17 +337,17 @@ void SystemClock_Config(void)
     }
 }
 
-void MX_UART4_Init(void)
+void MX_USART1_UART_Init(void)  // 改为USART1初始化函数
 {
-    huart4.Instance = UART4;
-    huart4.Init.BaudRate = 115200;
-    huart4.Init.WordLength = UART_WORDLENGTH_8B;
-    huart4.Init.StopBits = UART_STOPBITS_1;
-    huart4.Init.Parity = UART_PARITY_NONE;
-    huart4.Init.Mode = UART_MODE_TX_RX;
-    huart4.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-    huart4.Init.OverSampling = UART_OVERSAMPLING_16;
-    if (HAL_UART_Init(&huart4) != HAL_OK) {
+    UART1_Handler.Instance = USART1;  // 改为USART1
+    UART1_Handler.Init.BaudRate = 115200;
+    UART1_Handler.Init.WordLength = UART_WORDLENGTH_8B;
+    UART1_Handler.Init.StopBits = UART_STOPBITS_1;
+    UART1_Handler.Init.Parity = UART_PARITY_NONE;
+    UART1_Handler.Init.Mode = UART_MODE_TX_RX;
+    UART1_Handler.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    UART1_Handler.Init.OverSampling = UART_OVERSAMPLING_16;
+    if (HAL_UART_Init(&UART1_Handler) != HAL_OK) {
         Error_Handler();
     }
 }
@@ -110,26 +358,26 @@ void MX_GPIO_Init(void)
 
     // 使能GPIO时钟
     __HAL_RCC_GPIOA_CLK_ENABLE();
-    __HAL_RCC_GPIOC_CLK_ENABLE();
     __HAL_RCC_GPIOG_CLK_ENABLE();
+    __HAL_RCC_GPIOF_CLK_ENABLE();
 
-    // 配置PA2为UART4_TX
-    GPIO_InitStruct.Pin = GPIO_PIN_2;
+    // 配置PA9为USART1_TX
+    GPIO_InitStruct.Pin = GPIO_PIN_9;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF8_UART4;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART1;  // USART1的AF7
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-    // 配置PA3为UART4_RX  
-    GPIO_InitStruct.Pin = GPIO_PIN_3;
+    // 配置PA10为USART1_RX
+    GPIO_InitStruct.Pin = GPIO_PIN_10;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF8_UART4;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART1;  // USART1的AF7
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-    // 配置PG8为收发控制引脚
+    // 配置PG8为收发控制引脚（如果仍然需要RS485控制）
     GPIO_InitStruct.Pin = GPIO_PIN_8;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
@@ -138,6 +386,16 @@ void MX_GPIO_Init(void)
 
     // 初始状态设为接收模式
     HAL_GPIO_WritePin(GPIOG, GPIO_PIN_8, GPIO_PIN_RESET);
+
+    // 配置PF9和PF10为推挽输出，初始化LED熄灭
+    GPIO_InitStruct.Pin = GPIO_PIN_9 | GPIO_PIN_10;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+    // 初始关闭LED
+    HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9 | GPIO_PIN_10, GPIO_PIN_RESET);
 }
 
 void Error_Handler(void)
@@ -149,21 +407,13 @@ void Error_Handler(void)
     }
 }
 
-// 重定向printf到UART
+// 重定向printf到USART1
 int _write(int file, char *ptr, int len)
 {
-    HAL_UART_Transmit(&huart4, (uint8_t*)ptr, len, 1000);
+    HAL_UART_Transmit(&UART1_Handler, (uint8_t*)ptr, len, 1000);  // 改为UART1_Handler
     return len;
 }
 
-void HAL_UART_MspInit(UART_HandleTypeDef* huart)
-{
-    if(huart->Instance == UART4)
-    {
-        __HAL_RCC_UART4_CLK_ENABLE();
-        __HAL_RCC_GPIOA_CLK_ENABLE();
-    }
-}
 
 #ifdef USE_FULL_ASSERT
 void assert_failed(uint8_t *file, uint32_t line)
